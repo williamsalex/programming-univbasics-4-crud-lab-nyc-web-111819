1c88d328c9f21d9341b3494de256a355f73f4d0a
Alexander Williams <williams_alex@protonmail.com>
2019-10-04 18:13:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,11 +20,11 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array.delete_at(0)
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array.
 end
 
 def retrieve_first_element_from_array(array)
